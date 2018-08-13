3b10d30109b5639231caeb9ebac0b4486469ccc9
Krystal Foster and Nikki Wong <nikki@>
2018-08-13 13:04:05 -0700

    hide elements in css and add show method in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
+    $(".well").show();
     var items = ["item1", "item2","item3", "item4", "item5"];
 
     var groceries = items.map(function(item) {
         return $("input#" + item).val().toUpperCase();
     });
-
     groceries.sort().map(function(grocery, index) {
       $("." + items[index]).text(grocery);
     });
+
   });
 });
